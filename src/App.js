eefe49a07a4f421cd303450576f23264fe18f573
miraj <miraj00@gmail.com>
2021-10-30 00:13:55 -0400

    Contact Form added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import About from './components/About';
 import Nav from './components/Nav';
-import Gallery from "./components/Gallery";
+import About from './components/About';
+import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
-
   const [categories] = useState([
-    { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects',},
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
     { name: 'portraits', description: 'Portraits of people in my life' },
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
@@ -16,20 +17,27 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
-     <Nav 
+      <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory} >  </Nav>
-     
-     <main>
-        <div>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
-     </main>
-
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+          )}
+      </main>
     </div>
   );
 }
