6dba2364177820340fa126be83bc61a67f36778f
miraj <miraj00@gmail.com>
2021-10-30 22:16:27 -0400

    Photo modal added

--- src/components/Contact/__tests__/index.test.js
+++ src/components/Contact/__tests__/index.test.js
@@ -6,10 +6,10 @@ import Contact from '..'
 
 afterEach(cleanup)
 
-describe('Contact component renders', () => {
+describe('Contact component', () => {
   it('renders', () => {
-  render(<Contact />);
-});
+    render(<Contact />);
+  });
 
   it('renders', () => {
     const { asFragment } = render(<Contact />)
@@ -22,7 +22,7 @@ it('renders', () => {
   const { getByTestId } = render(<Contact />)
   expect(getByTestId('h1tag')).toHaveTextContent('Contact me')
 })
- 
+
 it('renders', () => {
   const { getByTestId } = render(<Contact />)
   expect(getByTestId('button')).toHaveTextContent('Submit')
